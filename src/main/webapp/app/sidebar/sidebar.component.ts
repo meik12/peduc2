ff6535074e69cdbc98f312ca15b452a5a0a2261c
unknown <mikiyassemu@gmail.com>
2018-12-08 20:50:03 -0800

    all

--- src/main/webapp/app/sidebar/sidebar.component.ts
+++ src/main/webapp/app/sidebar/sidebar.component.ts
@@ -1,12 +1,15 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit, Inject } from '@angular/core';
+import { DOCUMENT } from '@angular/common';
 @Component({
     selector: 'jhi-sidebar',
     templateUrl: './sidebar.component.html',
     styleUrls: ['./sidebar.component.scss']
 })
 export class SidebarComponent implements OnInit {
-    constructor() {}
+    constructor(@Inject(DOCUMENT) private document: any) {}
 
     ngOnInit() {}
+    goToFacebook(): void {
+        this.document.location.href = 'https://www.facebook.com/';
+    }
 }
